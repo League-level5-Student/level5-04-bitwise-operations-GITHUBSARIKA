63f81bc89fe6e8ccdb0543cdaed6da0bbe9d8fa5
root <root@1b7a04ae5d83>
2021-03-05 02:21:44 +0000

    yu6tujnt5yh5yh

--- src/_05_Pseudo_RNG/PseudoRNG.java
+++ src/_05_Pseudo_RNG/PseudoRNG.java
@@ -10,9 +10,11 @@ public class PseudoRNG {
 	 */
 	
 	//1. Create a member variable of type long called "seed"
-	
+	long seed;
 	//2. Make a constructor that initializes the memeber variable
-	
+	public PseudoRNG(long seed) {
+		this.seed=seed;
+	}
 	//3. Complete the steps inside the xorShift method
 	
 	//4. Complete the steps in the main method
@@ -27,27 +29,28 @@ public class PseudoRNG {
 	long xorShift(){
 		//1. Create a local long called seed and set it equal to the member variable 
 		//   We will only use the local variable for the rest of the method
-		
+		long seeds=seed;
 		//2. Shift seed to the left 13 bits and save the result
-		
+		long a=seed<<13;
 		//3. xor seed by the result of step 1 and save that result
-		
+		long b=seeds^a;
 		//4. Set seed equal to the result of step 2
-	
+	seeds=b;
 		//5. Shift seed to the right 7 bits and save the result
-		
+		long c=seeds>>7;
 		//6. xor seed by the result of step 4 and save that result
-				
+				long d=seeds^b;
 		//7. Set seed equal to the result of step 5
-		
+		seeds=c;
 		//8. Shift seed to the left 17 bits and save the result
-		
+		long e=seeds<<17;
 		//9. xor seed by the result of step 7 and save that result
-				
+				long f=seeds^c;
 		//10. Set seed equal to the result of step 8
-		
+		seeds=e;
 		//11. change the line below to return seed
-		return 0;
+		seed = seeds;
+		return seeds;
 		
 		//For fun, see if you can rewrite the method using only 4 lines of code
 	}
@@ -57,15 +60,23 @@ public class PseudoRNG {
 	public static void main(String[] args) {
 	
 		//1. Create a PseudoRNG object with any seed. 
-		
+		long x=6;
+		PseudoRNG yay=new PseudoRNG(System.currentTimeMillis());
 		//2. Print the result of xorShift() 3 times and note the random numbers. 
+		System.out.println(yay.xorShift());
+		System.out.println(yay.xorShift());
+		System.out.println(yay.xorShift());
+		System.out.println(yay.xorShift());
+		
+	
 		
 		//3. Create a secon PSeudoRNG object with a different seed and print the result of xorShift 3 times.
-		
+		long y=10;
+		PseudoRNG yayyay=new PseudoRNG(y);
 		//4. Run the tests a second time and notice that 3 random numbers are the same because the seed generates the same sequence of random numbers.
-		
+		System.out.println(yayyay.xorShift());
 		//If you would like to set the seed with a number that is hard to predict, you can use System.currentTimeMillis().
-	
+	System.currentTimeMillis();
 	}
 }
 
